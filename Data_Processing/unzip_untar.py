f99362a41d614d2e65345a84cf58fdc481b7c583
Shahidullah Dost <shahidullahdost@Shahidullahs-MacBook-Pro.local>
2025-09-28 19:41:43 -0400

    Updates to Unzip Untar to handle all files in a folder

--- Data_Processing/unzip_untar.py
+++ Data_Processing/unzip_untar.py
@@ -11,3 +11,20 @@ def untar_file(tar_path, put_tars_here):
     os.makedirs(put_tars_here, exist_ok=True)
     with tarfile.open(tar_path, 'r') as tar_ref:
         tar_ref.extractall(put_tars_here)
+
+def unzip_folder(zip_folder, put_zips_here):
+    os.makedirs(put_zips_here, exist_ok=True)
+    for item in os.listdir(zip_folder):
+        if item.endswith('.zip'):
+            zip_path = os.path.join(zip_folder, item)
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(put_zips_here)
+
+def untar_folder(tar_folder, put_tars_here):
+    os.makedirs(put_tars_here, exist_ok=True)
+    for item in os.listdir(tar_folder):
+        if item.endswith('.tar'):
+            tar_path = os.path.join(tar_folder, item)
+            with tarfile.open(tar_path, 'r') as tar_ref:
+                tar_ref.extractall(put_tars_here)
+    
--- Data_Processing/use_case_unzip_untar.py
+++ Data_Processing/use_case_unzip_untar.py
@@ -1,4 +1,7 @@
-from unzip_untar import unzip_file, untar_file
+from unzip_untar import *
 
 untar_file('/Users/shahidullahdost/Documents/CS98/Word_Prediction/Dataset/Audio_Only_25kHz/s1.tar', '/Users/shahidullahdost/Documents/CS98/Word_Prediction/Data_Processing/Audio_Only_25kHz/s1')
 unzip_file('/Users/shahidullahdost/Documents/CS98/Word_Prediction/Dataset/Video_Normal_Quality/s1.mpg_vcd.zip', '/Users/shahidullahdost/Documents/CS98/Word_Prediction/Data_Processing/Video_Normal_Quality/s1')
+
+untar_folder('/Users/shahidullahdost/Documents/CS98/Word_Prediction/Dataset/Audio_Only_25kHz', '/Users/shahidullahdost/Documents/CS98/Word_Prediction/Data_Processing/Unzip_All_Audio')
+unzip_folder('/Users/shahidullahdost/Documents/CS98/Word_Prediction/Dataset/Video_Normal_Quality', '/Users/shahidullahdost/Documents/CS98/Word_Prediction/Data_Processing/Unzip_All_Video')
